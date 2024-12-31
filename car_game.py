74c01130a100565951005afb145d9ba57b0d133c
Muhammad <muhammadmalik5303@gmail.com>
2025-01-01 01:56:48 +0500

    Updated

--- car_game.py
+++ car_game.py
@@ -1,2 +1,17 @@
 import pygame
+from pygame.locals import *
 
+
+# initializing the pygame application
+pygame.init()
+running = True
+screen = pygame.display.set_mode((800, 800)) # Method Args: (width, height)
+
+while running:
+    for event in pygame.event.get(): # fetching all events from the pygame.event module with the get method and iterating over them
+        if event.type == QUIT:
+            running = False
+
+
+# collapsing the application window
+pygame.quit()
