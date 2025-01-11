6e677e6f3c08a70065ede2c1ac3c6a3fd88e07d6
Muhammad <muhammadmalik5303@gmail.com>
2025-01-12 02:10:28 +0500

    Update car_game.py

--- car_game.py
+++ car_game.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 
 size = width, height = (800, 800) # Unpacking the tuple into width and height of the screen
 road_w = int(width/1.6) # Set road width to 500 pixels (800/1.6), also convert the resulting floating point to an integer
+roadmark_w = int(width/80) # Set roadmarking width to 10 pixels (800/80)
 
 # Initialize the pygame application
 pygame.init()
@@ -13,14 +14,42 @@ screen = pygame.display.set_mode(size)
 # Set title
 pygame.display.set_caption('Hot Wheels')
 # Set background color
-screen.fill((20,50,20)) # (red, green, blue)
+screen.fill((1, 115, 92)) # (red, green, blue)
+
+# Graphics - Draw the road
 pygame.draw.rect(
     screen,               # Drawing surface
     (50, 50, 50),         # Color: Dark Grey
     (width/2 - road_w/2,  # x-coordinate: Centered horizontally
      0,                   # y-coordinate: Starts at the top of the screen
-     road_w,              # Width of the rectangle
-     height)              # Height of the rectangle (full screen height)
+     road_w,              # Width of the road
+     height)              # Height of the road (full screen height)
+)
+#Draw the central yellowish road marking
+pygame.draw.rect(
+    screen,
+    (255, 240, 60),           # Color: Yellow
+    (width/2 - roadmark_w/2,  # x-coordinate: Centered horizontally
+      0,                      # y-coordinate: Starts at the top of the screen
+      roadmark_w,             # Width of the roadmark
+      height)                 # Height of the roadmark
+)
+# Draw the white markings on both edges of the road
+pygame.draw.rect( # left white marking
+    screen,
+    (255, 255, 255),                      # Color: White
+    (width/2 - road_w/2 + roadmark_w * 2, # (width/2 - road_w/2) starts from the left edge of the road and (+ roadmark_w * 2) takes it 20 px away from that edge
+      0,                                  # y-coordinate: Starts at the top of the screen
+      roadmark_w,                         # Width of the roadmark
+      height)                             # Height of the roadmark
+)
+pygame.draw.rect( # right white marking
+    screen,
+    (255, 255, 255),                      # Color: White
+    (width/2 + road_w/2 - roadmark_w * 3, # reverse the signs to mirror the effect on the other side
+      0,                                  # y-coordinate: Starts at the top of the screen
+      roadmark_w,                         # Width of the roadmark
+      height)                             # Height of the roadmark
 )
 # Apply the changes on the screen
 pygame.display.update()
